f4dde37db970ae1dbb31f4b13d9d56a65769401d
duyhung141 <leduyhung141@gmail.com>
2023-05-27 19:14:56 +0700

    fix input cart

--- src/page/Cart/Cart.jsx
+++ src/page/Cart/Cart.jsx
@@ -139,8 +139,8 @@ return (
                         </select>
                     </div>
                     <div className="">
-                        <label className="font-medium inline-block mb-3 text-sm uppercase">Địa chỉ (Số nhà, Phố)</label>
-                        <input id="email" name="email" type="email" autoComplete="email" required
+                        <label htmlFor="address" className="font-medium inline-block mb-3 text-sm uppercase">Địa chỉ (Số nhà, Phố)</label>
+                        <input id="address" name="address" type="text" autoComplete="address" required
                                className="block w-full rounded-md border-0 p-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300
                                        placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
                     </div>

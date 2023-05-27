a6f15f8a5a1f5ce0305c4230e41312093a6252db
duyhung141 <leduyhung141@gmail.com>
2023-05-27 15:25:08 +0700

    update header

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,15 @@
 import HomePage from "../page/HomePage/Homepage";
-
+import Login from "../page/Login/Login";
 
 export const router = [
     {
         path: '/',
         page: HomePage,
+        isShowHeader: true
+    },
+    {
+        path: '/login',
+        page: Login,
         isShowHeader: true
     },
    

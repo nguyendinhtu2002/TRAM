e695fc6ee1ec70da481465ced5efb7bf2a87347e
duyhung141 <leduyhung141@gmail.com>
2023-05-29 10:10:28 +0700

    add address payment to account

--- src/page/Account/Account.jsx
+++ src/page/Account/Account.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 function Account() {
+    const [firstName,setFirstName] = useState('')
+    const [lastName,setLastName] = useState('')
+    const [email,setEmail] = useState('')
+    const [password,setPassword] = useState('')
+    const [newPassword,setNewPassword] = useState('')
+    const [confirmPassword,setConfirmPassword] = useState('')
+    const [phone,setPhone] = useState('')
+    const [nameReceipt,setNameReceipt] = useState('')
+    const [address,setAddress] = useState('')
+    const [city,setCity] = useState('')
+    const [district,setDistrict] = useState('')
+
     return (
-        <div>
+        <div className="">
             <div className="bg-gray-200 mb-5">
                 <h1 className="uppercase text-3xl font-bold px-20 py-8">Tài khoản</h1>
             </div>
-            <div className="container mx-auto">
-                <form className="mx-auto w-full max-w-lg">
+            <div className="container mx-auto lg:flex lg:gap-10">
+                <form className="mx-auto w-full max-w-lg mb-10">
                     <h2 className="uppercase font-bold text-red-600 my-4">Thông tin cá nhân</h2>
                     <div className="flex flex-wrap -mx-3 mb-6">
                         <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
@@ -18,7 +30,7 @@ function Account() {
                             <input
                                 className="block w-full rounded-md border-0 p-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300
                                        placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                id="grid-first-name" type="text"/>
+                                id="grid-first-name" type="text" onChange={(e)=>setFirstName(e.target.value)}/>
                         </div>
                         <div className="w-full md:w-1/2 px-3">
                             <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
@@ -28,7 +40,7 @@ function Account() {
                             <input
                                 className="block w-full rounded-md border-0 p-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300
                                        placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                id="grid-last-name" type="text"/>
+                                id="grid-last-name" type="text" onChange={(e)=>setLastName(e.target.value)}/>
                         </div>
                     </div>
                     <div className="flex flex-wrap -mx-3 mb-6">
@@ -40,7 +52,7 @@ function Account() {
                             <input
                                 className="block w-full rounded-md border-0 p-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300
                                        placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                id="grid-email" type="email"/>
+                                id="grid-email" type="email"  onChange={(e)=>setEmail(e.target.value)}/>
                         </div>
                     </div>
                     <h2 className="uppercase font-bold text-red-600 my-4">Thay đổi mật khẩu</h2>
@@ -54,7 +66,7 @@ function Account() {
                             <input
                                 className="block w-full rounded-md border-0 p-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300
                                        placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                id="grid-password" type="password" placeholder="******************"/>
+                                id="grid-password" type="password" placeholder="******************" onChange={(e)=>setPassword(e.target.value)}/>
                         </div>
                     </div>
                     <div className="flex flex-wrap -mx-3 mb-6">
@@ -66,7 +78,7 @@ function Account() {
                             <input
                                 className="block w-full rounded-md border-0 p-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300
                                        placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                id="grid-new-password" type="password" placeholder="******************"/>
+                                id="grid-new-password" type="password" placeholder="******************" onChange={(e)=>setNewPassword(e.target.value)}/>
                         </div>
                     </div>
                     <div className="flex flex-wrap -mx-3 mb-6">
@@ -78,9 +90,87 @@ function Account() {
                             <input
                                 className="block w-full rounded-md border-0 p-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300
                                        placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                id="grid-confirm-password" type="password" placeholder="******************"/>
+                                id="grid-confirm-password" type="password" placeholder="******************" onChange={(e)=>setConfirmPassword(e.target.value)}/>
                         </div>
                     </div>
+                    <button
+                            className="bg-indigo-500 flex w-full justify-center rounded-md px-3 py-1.5 text-sm font-semibold
+                                    leading-6 text-white shadow-sm
+                                    hover:bg-indigo-300 focus-visible:outline focus-visible:outline-2
+                                    focus-visible:outline-offset-2 focus-visible:outline-indigo-600" >Lưu thông tin tài khoản
+                    </button>
+                </form>
+                <form className="mx-auto w-full max-w-lg mb-10">
+                    <h2 className="uppercase font-bold text-red-600 my-4">Địa chỉ thanh toán</h2>
+                    <div className="flex flex-wrap -mx-3 mb-6">
+                        <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                                   htmlFor="grid-phone">
+                                Số điện thoại *
+                            </label>
+                            <input
+                                className="block w-full rounded-md border-0 p-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300
+                                       placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                id="grid-phone" type="text" onChange={(e)=>setPhone(e.target.value)}/>
+                        </div>
+                        <div className="w-full md:w-1/2 px-3">
+                            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                                   htmlFor="grid-name-receipt">
+                                Tên người nhận
+                            </label>
+                            <input
+                                className="block w-full rounded-md border-0 p-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300
+                                       placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                id="grid-name-receipt" type="text" onChange={(e)=>setNameReceipt(e.target.value)}/>
+                        </div>
+                    </div>
+                    <div className="flex flex-wrap -mx-3 mb-6">
+                        <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                                Thành phố
+                            </label>
+                            <select className="block w-full rounded-md border-0 p-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300
+                                       placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    onChange={(e) => {
+                                        setCity(e.target.value);
+                                    }}>>
+                                <option>Hà Nội</option>
+                                <option>Hồ Chí Minh</option>
+                            </select>
+                        </div>
+                        <div className="w-full md:w-1/2 px-3">
+                            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                                Quận/Huyện
+                            </label>
+                            <select className="block w-full rounded-md border-0 p-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300
+                                       placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    onChange={(e) => {
+                                        setDistrict(e.target.value);
+                                    }}>
+
+                                <option value="Hoàn Kiếm">Hoàn Kiếm</option>
+                                <option value="Đông Anh">Đông Anh</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div className="flex flex-wrap -mx-3 mb-6">
+                        <div className="w-full px-3">
+                            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                                   htmlFor="grid-address">
+                                Địa chỉ
+                            </label>
+                            <input
+                                className="block w-full rounded-md border-0 p-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300
+                                       placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                id="grid-address" type="text" onChange={(e)=>setAddress(e.target.value)}/>
+                        </div>
+                    </div>
+                    <button
+                        className="bg-indigo-500 flex w-full justify-center rounded-md px-3 py-1.5 text-sm font-semibold
+                                    leading-6 text-white shadow-sm
+                                    hover:bg-indigo-300 focus-visible:outline focus-visible:outline-2
+                                    focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Lưu thông địa chỉ thanh toán
+                    </button>
                 </form>
             </div>
 

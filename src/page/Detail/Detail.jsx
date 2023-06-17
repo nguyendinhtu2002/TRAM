69111037b19e510e4b27a60a250dfb78de4ac8d2
duyhung141 <leduyhung141@gmail.com>
2023-06-17 16:32:22 +0700

    add toast before add to cart

--- src/page/Detail/Detail.jsx
+++ src/page/Detail/Detail.jsx
@@ -60,7 +60,9 @@ function Detail() {
   const [count, setCount] = useState(1);
   const [isSelected, setSelected] = useState(false);
   const [checkBeforeAdd,setCheckBeforeAdd] = useState(null);
-  const handleGetDetailProduct = async (id) => {
+    const [showNotification, setShowNotification] = useState(false);
+
+    const handleGetDetailProduct = async (id) => {
     const res = await ProductService.getDetail(id);
 
     return res;
@@ -89,11 +91,11 @@ function Detail() {
           setCheckBeforeAdd(false);
           const updatedData = { ...data, quantityOrder: count };
           dispatch(addCart(updatedData));
+          setShowNotification(true);
       }
       else{
           setCheckBeforeAdd(true);
       }
-
   };
   const CommentItem = () => {
     const [reply, setReply] = useState(null);
@@ -305,18 +307,7 @@ function Detail() {
                           hàng từ 1.590.000
                         </p>
                       ))}
-                      {/* <p className="text-sm">
-                        <span className="text-[#F99B1C] font-bold">
-                          100k2590
-                        </span>{" "}
-                        – Giảm 100.000 Cho đơn hàng từ 2.590.000{" "}
-                      </p>
-                      <p className="text-sm">
-                        <span className="text-[#F99B1C] font-bold">
-                          200k4790{" "}
-                        </span>{" "}
-                        – Giảm 200.000 Cho đơn hàng từ 4.790.000
-                      </p> */}
+
                     </div>
                   </div>
 
@@ -328,7 +319,7 @@ function Detail() {
                       className="flex text-white bg-cyan-500  border-0 py-2 px-6 focus:outline-none hover:bg-cyan-600 rounded"
                       onClick={handleAddCart}
                     >
-                      Add to cart
+                      Thêm vào giỏ hàng
                     </button>
                     <button
                       className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 "
@@ -346,6 +337,36 @@ function Detail() {
                       </svg>
                     </button>
                   </div>
+                    {showNotification&&(
+                        <div id="toast-success"
+                             className="absolute flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800"
+                             role="alert">
+                            <div
+                                className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg dark:bg-green-800 dark:text-green-200">
+                                <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                                     xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd"
+                                          d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                                          clip-rule="evenodd"></path>
+                                </svg>
+                                <span className="sr-only">Check icon</span>
+                            </div>
+                            <div className="ml-3 text-sm font-normal">Item moved successfully.</div>
+                            <button type="button"
+                                    className="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
+                                    data-dismiss-target="#toast-success" aria-label="Close"
+                                    onClick={() => setShowNotification(false)}
+                            >
+                                <span className="sr-only">Close</span>
+                                <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                                     xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd"
+                                          d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                          clip-rule="evenodd"></path>
+                                </svg>
+                            </button>
+                        </div>
+                    )}
                 </div>
               </div>
             </div>

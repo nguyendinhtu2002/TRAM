ed93be09c2ff271702466dd7515b510c248d31df
duyhung141 <leduyhung141@gmail.com>
2023-06-18 00:09:14 +0700

    udpate cart

--- src/page/Cart/Cart.jsx
+++ src/page/Cart/Cart.jsx
@@ -57,7 +57,7 @@ function Cart() {
             <HeaderComponent></HeaderComponent>
             <div className="container mx-auto mt-10">
                 <div className="lg:flex shadow-md my-10">
-                    <div className="lg:w-3/4 bg-white px-10 py-10">
+                    <div className="lg:w-full bg-white px-10 py-10">
                         <div className="flex justify-between border-b pb-8">
                             <h1 className="font-semibold text-2xl">Giỏ hàng</h1>
                             <h2 className="font-semibold text-2xl capitalize">{cart.length} sản phẩm</h2>
@@ -120,15 +120,14 @@ function Cart() {
 
                         }
 
-
-                        <a href="#" className="flex font-semibold text-indigo-600 text-sm mt-10">
+                        <Link to={'/'} className="flex font-semibold text-indigo-600 text-sm mt-10">
 
                             <svg className="fill-current mr-2 text-indigo-600 w-4" viewBox="0 0 448 512">
                                 <path
                                     d="M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z" />
                             </svg>
                             Tiếp tục mua hàng
-                        </a>
+                        </Link>
                     </div>
                     {cart.cart.length > 0 &&(
                         <div id="summary" className="lg:w-1/4 px-8 py-10">

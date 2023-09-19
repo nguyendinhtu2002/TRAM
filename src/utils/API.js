9048f17b743373ac6ac82a6aeeab63d366f23db5
Nguyễn Đình Tư <nguyendinhtu111022002@gmail.com>
2023-09-20 00:33:33 +0700

    update

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,2 +1,2 @@
-// export const  URL  = "https://api.tramdinh.vn/"
-export const  URL  = "http://localhost:5000/"
+export const  URL  = "https://api.tramdinh.vn/"
+// export const  URL  = "http://localhost:5000/"
